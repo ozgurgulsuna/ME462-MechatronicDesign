a29fef4b20fe899d4d83c52feb1024594ae8e454
ozgur <ozgurgulsuna@gmail.com>
2022-05-11 23:08:08 +0300

    [FW] Remove Submodules

---
+++ 3.Firmware/EncoderPico/Dependencies/picotool/build/CMakeFiles/picotool.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/picotool.dir/main.cpp.o"
+  "CMakeFiles/picotool.dir/main.cpp.o.d"
+  "CMakeFiles/picotool.dir/picoboot_connection/picoboot_connection.c.o"
+  "CMakeFiles/picotool.dir/picoboot_connection/picoboot_connection.c.o.d"
+  "CMakeFiles/picotool.dir/picoboot_connection/picoboot_connection_cxx.cpp.o"
+  "CMakeFiles/picotool.dir/picoboot_connection/picoboot_connection_cxx.cpp.o.d"
+  "picotool"
+  "picotool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/picotool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
