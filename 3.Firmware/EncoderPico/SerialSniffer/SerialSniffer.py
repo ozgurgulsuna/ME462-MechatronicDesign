456b9f4270c9eec4d6e1868d8e5a4f7bd7c45040
ozgur <ozgurgulsuna@gmail.com>
2022-05-17 20:32:21 +0300

    [FW] Serial sniffer updated, plotter tested

--- 3.Firmware/EncoderPico/SerialSniffer/SerialSniffer.py
+++ 3.Firmware/EncoderPico/SerialSniffer/SerialSniffer.py
@@ -10,28 +10,10 @@ ser = serial.Serial(
     bytesize=serial.SEVENBITS
 )
 
-ser.isOpen()
 
-print ('Enter your commands below.\r\nInsert "exit" to leave the application.')
-
-input=1
 while 1 :
-    # get keyboard input
-    input = raw_input(">> ")
-        # Python 3 users
-        # input = input(">> ")
-    if input == 'exit':
-        ser.close()
-        exit()
-    else:
-        # send the character to the device
-        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-        ser.write(input + '\r\n')
-        out = ''
-        # let's wait one second before reading output (let's give device time to answer)
-        time.sleep(1)
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
-            
-        if out != '':
-            print (">>" + out) 
+    raw=ser.readline().decode()
+    pos=int(raw[9:17])
+    diff=int(raw[24:-2])
+
+    print(pos, ",",diff)
